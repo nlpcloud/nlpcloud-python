9d3cbae5fcf22649527d0b8090e2572acaa68244
Julien Salinas <all@juliensalinas.com>
2021-01-14 15:03:26 +0000

    Add poc for all endpoints.

--- spacycloud/__init__.py
+++ spacycloud/__init__.py
@@ -3,19 +3,36 @@ import requests
 
 class Client:
     def __init__(self, token):
-        self.token = token
-
-    def get(self, user_input):
-
-        headers = {
-            "Authorization": "Token " + self.token,
+        self.headers = {
+            "Authorization": "Token " + token,
         }
 
+    def entities(self, user_input):
         payload = {
             "text": user_input
         }
 
         r = requests.post(
-            "https://api.spacycloud.io/v1/en_core_web_sm/entities", json=payload, headers=headers)
+            "https://api.spacycloud.io/v1/en_core_web_sm/entities", json=payload, headers=self.headers)
 
-        print(r.text)
+        return r.json()
+
+    def dependencies(self, user_input):
+        payload = {
+            "text": user_input
+        }
+
+        r = requests.post(
+            "https://api.spacycloud.io/v1/en_core_web_sm/dependencies", json=payload, headers=self.headers)
+
+        return r.json()
+
+    def sentence_dependencies(self, user_input):
+        payload = {
+            "text": user_input
+        }
+
+        r = requests.post(
+            "https://api.spacycloud.io/v1/en_core_web_sm/sentence-dependencies", json=payload, headers=self.headers)
+
+        return r.json()
