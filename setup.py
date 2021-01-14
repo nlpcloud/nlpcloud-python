f593d32622a445f812dfb24f499391cafd5a4072
Julien Salinas <all@juliensalinas.com>
2021-01-14 13:59:53 +0000

    Set up dev env with remote containers in VScode.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spacycloud',
+    version='0.0.1',
+    description='A Python client for the spaCy Cloud API (https://docs.spacycloud.io)',
+    packages=['spacycloud'],
+    author='Julien Salinas',
+    author_email='all@juliensalinas.com',
+    keywords=['api', 'client', 'spacycloud'],
+    url='https://github.com/spacycloud/python-client'
+)
