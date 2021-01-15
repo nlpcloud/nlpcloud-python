2dfe11a54929e6f41a9427da478d6f356860936d
Julien Salinas <all@juliensalinas.com>
2021-01-15 10:35:52 +0000

    Increase Pypi version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spacycloud',
-    version='1.0.3',
+    version='1.0.4',
     description='Python client for the spaCy Cloud API',
     long_description="spaCy Cloud serves all the spaCy pre-trained models, and your own custom models, through a RESTful API ready for production.\n\nThis is the Python client for the API.\n\nMore details here: https://spacycloud.io\n\nDocumentation: https://docs.spacycloud.io\n\nGithub: https://github.com/spacycloud/spacycloud-python",
     packages=['spacycloud'],
