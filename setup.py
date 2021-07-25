8270931ed5578cd0c98515bb6a82c6169efd6b21
Julien Salinas <all@juliensalinas.com>
2021-07-25 07:09:27 +0000

    Increment version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nlpcloud',
-    version='1.0.11',
+    version='1.0.12',
     description='Python client for the NLP Cloud API',
     long_description="NLP Cloud serves high performance pre-trained or custom models for NER, sentiment-analysis, classification, summarization, text generation, question answering, machine translation, language detection, tokenization, POS tagging, and dependency parsing. It is ready for production, served through a REST API.\n\nThis is the Python client for the API.\n\nMore details here: https://nlpcloud.io\n\nDocumentation: https://docs.nlpcloud.io\n\nGithub: https://github.com/nlpcloud/nlpcloud-python",
     packages=['nlpcloud'],
