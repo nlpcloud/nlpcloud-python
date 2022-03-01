43241f0de26d7ae0e1a39d516c53a87386295aa6
Julien Salinas <all@juliensalinas.com>
2022-03-01 14:33:00 +0000

    Add new meta tags.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='nlpcloud',
     version='1.0.20',
     description='Python client for the NLP Cloud API',
-    long_description="NLP Cloud serves high performance pre-trained or custom models for NER, sentiment-analysis, classification, summarization, text generation, question answering, machine translation, language detection, tokenization, POS tagging, and dependency parsing. It is ready for production, served through a REST API.\n\nThis is the Python client for the API.\n\nMore details here: https://nlpcloud.io\n\nDocumentation: https://docs.nlpcloud.io\n\nGithub: https://github.com/nlpcloud/nlpcloud-python",
+    long_description="NLP Cloud serves high performance pre-trained or custom models for NER, sentiment-analysis, classification, summarization, text generation, question answering, machine translation, language detection, semantic similarity, tokenization, POS tagging, embeddings, and dependency parsing. It is ready for production, served through a REST API.\n\nThis is the Python client for the API.\n\nMore details here: https://nlpcloud.io\n\nDocumentation: https://docs.nlpcloud.io\n\nGithub: https://github.com/nlpcloud/nlpcloud-python",
     packages=['nlpcloud'],
     author='Julien Salinas',
     author_email='all@juliensalinas.com',
